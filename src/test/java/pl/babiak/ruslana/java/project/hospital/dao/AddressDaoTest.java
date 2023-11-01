6fb5ecb9d811b2413e5654e585f3264f7333388a
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-11-01 11:58:54 +0100

    Added Data Access Object for Address Model.

---
+++ src/test/java/pl/babiak/ruslana/java/project/hospital/dao/AddressDaoTest.java
@@ -0,0 +1,18 @@
+package pl.babiak.ruslana.java.project.hospital.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressDaoTest {
+
+    @Test
+    void list() {
+        //given
+        AddressDao addressDao = new AddressDao();
+        //when
+        addressDao.list();
+        //then
+
+    }
+}
