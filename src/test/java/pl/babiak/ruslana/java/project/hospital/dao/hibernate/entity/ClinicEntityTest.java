e65cf1e5d4d0ee981391b43eb2f3734295dc1313
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-11-27 12:13:21 +0100

    Added hibernate configuration with entity and test for Clinic model.

---
+++ src/test/java/pl/babiak/ruslana/java/project/hospital/dao/hibernate/entity/ClinicEntityTest.java
@@ -0,0 +1,48 @@
+package pl.babiak.ruslana.java.project.hospital.dao.hibernate.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicEntityTest {
+    private SessionFactory sessionFactory;
+
+    @BeforeEach
+    void setUp() {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry =
+                new StandardServiceRegistryBuilder()
+                        .configure("hibernate.cfg.xml")
+                        .build();
+        try {
+            sessionFactory =
+                    new MetadataSources(registry)
+                            .buildMetadata()
+                            .buildSessionFactory();
+        } catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we
+            // had trouble building the SessionFactory so destroy it manually.
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    @Test
+    void create() {
+        //given
+
+        //when
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+        session.save(new ClinicEntity("NFZ"));
+        session.getTransaction().commit();
+        //then
+
+    }
+
+}
