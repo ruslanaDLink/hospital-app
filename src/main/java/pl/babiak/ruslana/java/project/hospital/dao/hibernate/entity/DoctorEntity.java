942ab468914e52f5a1f55d27ca2599fab44d7c0c
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-12-04 15:26:11 +0100

    Added DoctorEntity class to entity package.

---
+++ src/main/java/pl/babiak/ruslana/java/project/hospital/dao/hibernate/entity/DoctorEntity.java
@@ -0,0 +1,60 @@
+package pl.babiak.ruslana.java.project.hospital.dao.hibernate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import pl.babiak.ruslana.java.project.hospital.model.DoctorType;
+
+@Entity
+public class DoctorEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "DOCTOR_TYPE")
+    private DoctorType type;
+
+
+    public DoctorEntity() {
+    }
+
+    public DoctorEntity(String name, DoctorType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DoctorType getType() {
+        return type;
+    }
+
+    public void setType(DoctorType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "DoctorEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
